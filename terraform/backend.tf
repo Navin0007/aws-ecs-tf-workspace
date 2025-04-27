4718789adffddd8a3aba086533869d1f4d3a0687
faangle <hr@faangpath.co.in>
2025-04-27 15:09:44 +0530

    backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-backend-bucket"
+    bucket         = "terraform-ecs-dem"
     key            = "ecs-workspace/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "your-terraform-lock-table"
+    dynamodb_table = "terraform-ecs-dem"
   }
 }
 
